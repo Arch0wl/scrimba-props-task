79e0fe39c8fedc1b13e92b95caeb06b677e6c2ba
Arch0wl <annstarovoitov@gmail.com>
2022-08-07 19:04:36 -0400

    changes to joke.js

--- src/Joke.js
+++ src/Joke.js
@@ -4,9 +4,9 @@ export default function Joke (props) {
  
   return (
     <div>
-      {props.setup && <h3>Setup: {props.setup}}</h3>}
-      <p>{props.punchline}</p>
-      <hr />
+         {props.setup && <h3>Setup: {props.setup}</h3>}
+            <p>Punchline: {props.punchline}</p>
+            <hr />
     </div>
   )
 }
